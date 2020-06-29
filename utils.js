fcc7c08f957ff8a48ed06447e025b90eb1594802
Juan Estrella <juan.estrella137@gmail.com>
2020-06-29 15:06:13 +0200

    Practice with OMDb API

---
+++ utils.js
@@ -0,0 +1,17 @@
+// De-bouncing an input
+// Stop fetch from sending request on every key press by setting
+// a timeout function to run X seconds after the last user input is received
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+export { debounce };
