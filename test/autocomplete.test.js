a2cfcbaa5b674317226c60ed2f489d503bdab808
Juan Estrella <juan.estrella137@gmail.com>
2020-07-12 13:30:16 +0200

    Test if the dropdown opens up on input by creating a fake DOM event

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,6 +1,7 @@
 const { createAutoComplete } = require('../autocomplete');
 
-it('Shows an autocomplete', () => {
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = '';
   createAutoComplete({
     root         : document.querySelector('#target'),
     fetchData() {
@@ -10,8 +11,20 @@ it('Shows an autocomplete', () => {
       return movie.Title;
     }
   });
+});
 
+it('Dropdown starts closed', () => {
   const dropdown = document.querySelector('.dropdown');
 
   expect(dropdown.className).not.to.include('is-active');
+});
+
+it('After searching, dropdown opens up', () => {
+  const input = document.querySelector('input');
+  input.value = 'Avengers';
+  input.dispatchEvent(new Event('input'));
+
+  const dropdown = document.querySelector('.dropdown');
+
+  expect(dropdown.className).to.include('is-active');
 });
