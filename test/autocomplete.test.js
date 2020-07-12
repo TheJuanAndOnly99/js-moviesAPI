20b2f552ba308524759b5881f8a90eb00be66bd8
Juan Estrella <juan.estrella137@gmail.com>
2020-07-12 13:12:38 +0200

    Test the autocomplete function using Mochajs in the browser

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,1 +1,13 @@
-it('works!!', () => {});
+const { createAutoComplete } = require('../autocomplete');
+
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root         : document.querySelector('#target'),
+    fetchData() {
+      return [ { Title: 'Avengers' }, { Title: 'The Dark Knight' }, { Title: 'Kung Fu Panda' } ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
+});
